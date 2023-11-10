2acf7927f517263c9e39144e75e47959c5e64de8
Israel Omola <izzyskills@gmail.com>
2023-11-10 21:26:13 +0100

    fixed some bugs

--- order/admin.py
+++ order/admin.py
@@ -1,6 +1,5 @@
 # Register your models here.
 from django.contrib import admin
+from . import models
 
-from .models import Order, OrderItem
-
-admin.site.register(Order)
+admin.site.register(models.Order)
